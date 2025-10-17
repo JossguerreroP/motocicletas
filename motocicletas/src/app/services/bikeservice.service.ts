c414073e872a1c91076618166cde3ec0bda5ca0f
JossguerreroP <josguerrerop@udistrital.edu.co>
2025-10-16 20:57:11 -0500

    service updatedservicebike

--- motocicletas/src/app/services/bikeservice.service.ts
+++ motocicletas/src/app/services/bikeservice.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BikeserviceService {
 
-  constructor() { }
+  private apiUrl = '';
+
+   constructor(private http: HttpClient) {}
+
+   getAllBikes(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
 }
